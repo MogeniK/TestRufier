f0fe458f6c477f6cd592b69fc5c6ca617177cec7
ziro123 <korolchuk.serg@gmail.com>
2022-10-09 19:27:05 +0300

    Complite All

--- SerthWindow.py
+++ SerthWindow.py
@@ -1,12 +1,11 @@
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication,QWidget,QLabel,QPushButton,QVBoxLayout,QHBoxLayout
+from PyQt5.QtWidgets import QApplication,QWidget,QLabel,QPushButton,QVBoxLayout,QHBoxLayout, QLineEdit
 from instr import *
 class SerthWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.set_apper()
         self.initUI()
-        self.connects()
         self.show()
 
     def set_apper(self):
@@ -15,6 +14,9 @@ class SerthWindow(QWidget):
         self.move(win_x, win_y)
 
     def initUI(self):
-        pass
-    def connects (self):
-        pass
+        self.lb_work = QLabel('Роботоспособность')
+        self.lb_index = QLabel('Штндекс')
+        self.v_line = QVBoxLayout()
+        self.v_line.addWidget(self.lb_index,alignment=Qt.AlignCenter)
+        self.v_line.addWidget(self.lb_work,alignment=Qt.AlignCenter)
+        self.setLayout(self.v_line)
